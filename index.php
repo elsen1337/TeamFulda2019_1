5191207f8100606b6b257ded08cd69229d93e9d6
Moritz <moritz.mrosek@informatik.hs-fulda.de>
2019-05-03 23:42:47 +0200

    Changed links to only consist of team member names + Added Head lines.

--- index.php
+++ index.php
@@ -4,17 +4,30 @@
 	<meta charset="8-utf"/>
 </head>
 <body>
+
+	<h2>Team Project</h2>
+	<h2>Summer 2019</h2>
+	<h2>TeamFulda2019_1</h2>
+
 <?php 
 
 //$directory = "C:/Users/fdai3744/Desktop/testing/";
 $directory = "./";
 $phpfiles = glob($directory . "*.html");
+$names = [
+	0 => "Manuel",
+	1 => "Michael",
+	2 => "Moritz",
+	3 => "Ramon",
+	4 => "Simon"
+];
+$i = 0;
 
 foreach ($phpfiles as $phpfile)
 {
 	//echo file_get_contents($phpfile);
-	echo '<p><a href="'.basename($phpfile).'">'.$phpfile.'Blubb</a></p>';
-	
+	echo '<p><a href="'.basename($phpfile).'">'.$names[i].'</a></p>';
+	$i++;
 }
 
  //echo file_get_contents("C:/Users/fdai3744/Desktop/testing/ramon.profile.html");
