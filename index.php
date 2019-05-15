d7568eee3cdd2076b4c0c30cebfabacb7265d43b
elsen1337 <lsohn1337@gmail.com>
2019-05-16 01:15:53 +0200

    added hoverFX for buttons and git-repo-link in site logo

--- index.php
+++ index.php
@@ -1,19 +1,19 @@
 <html>
 <head>
-	<title>Home</title>
-	<meta charset="8-utf"/>
-	<link rel="stylesheet" href="style.css">
+    <title>Home</title>
+    <meta charset="8-utf"/>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-	<div id="siteHead">
-		<img id="logo" src="Study-Home.png">
-		<h2>Team Project</h2>
-		<h2>Summer 2019 in Fulda</h2>
-		<h2>TeamFulda2019_1</h2>
-	</div>
-	<h3>Team Members:</h3>
+<div id="siteHead">
+    <a href="https://github.com/sl05/TeamFulda2019_1" target="_blank"><img id="logo" src="Study-Home.png" alt="Logo"/></a>
+    <h2>Team Project</h2>
+    <h2>Summer 2019 in Fulda</h2>
+    <h2>TeamFulda2019_1</h2>
+</div>
+<h3>Team Members:</h3>
 
-<?php 
+<?php
 
 //$directory = "C:/Users/fdai3744/Desktop/testing/";
 $directory = "./";
@@ -24,14 +24,14 @@ $i = 0;
 
 foreach ($phpfiles as $phpfile)
 {
-	//echo file_get_contents($phpfile);
-	if($i < $arrayLength){
-	echo '<p class="nameButton"><a href="' . basename($phpfile) . '">' . $names[$i] . '</a></p>';
-	$i++;
-	}
+    //echo file_get_contents($phpfile);
+    if($i < $arrayLength){
+        echo '<p class="nameButton"><a class="hoverFX" href="' . basename($phpfile) . '">' . $names[$i] . '</a></p>';
+        $i++;
+    }
 }
 
- //echo file_get_contents("C:/Users/fdai3744/Desktop/testing/ramon.profile.html");
+//echo file_get_contents("C:/Users/fdai3744/Desktop/testing/ramon.profile.html");
 
 
 ?>
