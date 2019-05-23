d8b7be7ac7aa2c78c6884f92672f2d91a4ddb1fb
Simon <simon@nb-simon>
2019-05-23 14:22:18 +0200

    Search AS First User UI

--- control/wohnung.php
+++ control/wohnung.php
@@ -10,73 +10,59 @@ require('../kernel/class-ui.php');
 header('Content-Type: text/html; charset=UTF-8');
 
 
-/*
-wohn_id	int(11) Auto-Inkrement	
-vm_id	int(11)	
-name	varchar(47)	
-beschr	varchar(3070)	
-plz	varchar(15)	
-ort	varchar(47)	
-preis	decimal(6,2)	
-entf_meter	int(11)	
-entf_min	int(11)
-*/
-
 ?><!DOCTYPE HTML><html><head>
-<title>HSF-MScAI-TP - StudyHome</title></head>
-<body><h1>Controlpanel</h1><form action="<?=$_SERVER['SCRIPT_NAME']?>" method="post"><?php
+<title>HSF-MScAI-TP - StudyHome</title>
+<link rel="stylesheet" href="../client/layout/base-messages.css">
+<link rel="stylesheet" href="control.css"></head>
+
+<body class="whn"><h1>Controlpanel</h1><form action="<?=$_SERVER['SCRIPT_NAME']?>" method="post"><?php
 
 
 GUI::printNotice('Wohnungen');
 
 
 $pkey='wohn_id';
-$type=array('name'=>'text','beschr'=>'area','vm_id'=>'selection','plz'=>'number','ort'=>'text','preis'=>'number=step>0.01','entf_meter'=>'number','entf_min'=>'number');
+$type=array('visible'=>'selection','name'=>'text','beschr'=>'area','vm_id'=>'selection','str'=>'text','plz'=>'number','ort'=>'text','preis'=>'number=step>0.01','entf_meter'=>'number','entf_min'=>'number');
 
-FormFV::updateDB($_POST,$type,'new','wohnung',$pkey);
+FormFV::updateDB($_POST,$type,'new','wohnung',$pkey,'wdel');
 
-#var_dump($msdb);
 
 
 if (strlen($_GET['edit']) > 0) {
 
 
-$vmlist=FormFV::getSelectAdv('SELECT vm_id, CONCAT(nname, ", ", vname) AS label FROM vermieter');
+	$vmlist=FormFV::getSelectAdv('SELECT vm_id, CONCAT(nname, ", ", vname) AS label FROM vermieter');
 
-$sql='SELECT w.* FROM wohnung AS w WHERE wohn_id="'.$_GET['edit'].'"';
-$mrs=$msdb->query($sql); if ($mrs->num_rows > 0) {$row=$mrs->fetch_assoc();} else {$row[$pkey]='new';}
+	$sql='SELECT w.* FROM wohnung AS w WHERE wohn_id="'.$_GET['edit'].'"';
+	$mrs=$msdb->query($sql); if ($mrs->num_rows > 0) {$row=$mrs->fetch_assoc();} else {$row[$pkey]='new';}
 
-echo '<table>';
-echo FormFV::printVertical(FormFV::makeHTML($row,$type,$row[$pkey],array('vm_id'=>$vmlist),true,false),array('Bezeichnung','Beschreibung','Vermieter','ZIPCode','Ort','Preis','Entfernung zur UNI in Metern','Entfernung zur UNI in Minuten per ÖPNV'));
-echo '</table><p><input type="submit" value="Erstellen &middot; Aktualisieren"></p>';
+	echo '<table>';
+	echo FormFV::printVertical(FormFV::makeHTML($row,$type,$row[$pkey],array('vm_id'=>$vmlist,'visible'=>array(0=>'Zurückgehalten',1=>'Sichtbar')),true,false),array('Status','Bezeichnung','Beschreibung','Vermieter','Straße','PLZ/ZIP','Stadt/Ort','Preis/Woche','Entfernung zum Campus (Metern)','ÖPNV-Minuten zum Campus'));
+	echo '</table><p><input type="submit" value="Erstellen &middot; Aktualisieren"></p>';
 
 
 
 } else {
 
 
-$sql='SELECT w.*, v.* FROM wohnung AS w JOIN vermieter AS v ON w.vm_id=v.vm_id'; $msr=$msdb->query($sql);
-echo '<table cellpadding="2" style="empty-cells:show"><tr><th class="trash">&#x232B;</th><th>Bezeichnung</th><th>Vermieter</th><th>Optionen</th></tr>';
+	$sql='SELECT w.*, v.* FROM wohnung AS w JOIN vermieter AS v ON w.vm_id=v.vm_id'; $msr=$msdb->query($sql);
+	echo '<table cellpadding="2" style="empty-cells:show"><tr><th>Bezeichnung</th><th>Vermieter</th><th>Optionen</th></tr>';
 
-$sql='SELECT w.wohn_id, w.name, nname, vname FROM wohnung AS w JOIN vermieter AS v ON w.vm_id=v.vm_id';
-$msr=$msdb->query($sql); echo $msdb->error;
+	$sql='SELECT w.wohn_id, w.visible, w.name, nname, vname FROM wohnung AS w JOIN vermieter AS v ON w.vm_id=v.vm_id ORDER BY visible DESC';
+	$msr=$msdb->query($sql); echo $msdb->error;
 
-while ($row=$msr->fetch_assoc()) {
-	
-	echo '<tr><td><td>'.$row['name'].'</td><td>'.$row['nname'].', '.$row['vname'].'</td><td>'.$row['cnt'].'</td><td><a href="?edit='.$row[$pkey].'">Bearbeiten</a>'."</td></tr>\n";
-	
+	while ($row=$msr->fetch_assoc()) {
+		
+		echo '<tr><td class="'.($row['visible'] > 0 ? '' : 'hidden').'">'.$row['name'].'</td><td>'.$row['nname'].', '.$row['vname'].'</td><td><a href="?edit='.$row[$pkey].'">Bearbeiten</a> &middot; <a href="?wdel[]='.$row[$pkey].'">Löschen</a>'."</td></tr>\n";
+		
+	}
+
+	echo '</table>';
+
+	GUI::printInsert('<a href="?edit=new">Erstelle neue Wohnung</a>');
+
 }
 
-echo '</table>';
-
-GUI::printInsert('<a href="?edit=new">Erstelle neue Wohnung</a>');
-
-}
-
-?></table>
-
-
-
-</form>
+?></form>
 
 </body></html>
---
+++ index.php
@@ -0,0 +1,179 @@
+ <?php
+
+//require('core-auth.php');
+require('./core-mysqla.php');
+
+#require('./kernel/class-formhfv.php');
+require('./kernel/class-ui.php');
+
+session_start();
+
+header('Content-Type: text/html; charset=UTF-8');
+
+
+
+#phpinfo();
+#error_reporting(E_ALL);
+#ini_set('display_errors', 1);
+
+
+$searchKeyGlobal='appsearch';
+$searchKeyDistMtr='distmeter';
+$searchKeyDistLPT='distopnv';
+$searchKeyPrice='price';
+
+
+?><!DOCTYPE HTML><html><head>
+<title>HSF-MScAI-TP - StudyHome</title>
+<link rel="stylesheet" href="../client/layout/base-messages.css">
+<style type="text/css">
+fieldset {display:inline-block; width:230px}
+fieldset input[type='number'] {width:80px !important}
+table.resultset a {font-size: 1.35em}
+div#header {height:175px;margin-bottom:25px; border-bottom: 6px double silver; padding-bottom:25px} 
+div#header img {display:block; margin-right:25px; float: left; height: inherit; margin-bottom: -85px;}
+div#footer {border-top: 6px double silver; margin-top:25px;padding-top:10px}
+</style>
+</head>
+
+<body class="">
+
+
+
+<div id="header">
+
+<img src="client/image/logo-studyhome.png">
+
+<div style="clear: both; margin-left:300px">
+<h1>Fulda Software Engineering Project, Spring 2019. For Demonstration Only</h1>
+<h2>StudyHome - Find your Appartment for a Semester abroad in Fulda</h2>
+</div>
+
+</div>
+
+
+
+
+
+
+<div id="filtermenu">
+
+
+<form action="<?=$_SERVER['SCRIPT_NAME']?>" method='post'>
+
+
+<fieldset><legend>Distance to Campus (Meter)</legend>
+<input type="number" name="<?=$searchKeyGlobal.'['.$searchKeyDistMtr.'][Min]'?>" value="<?=$_REQUEST[$searchKeyGlobal][$searchKeyDistMtr]['Min']?>" placeholder="Min">
+-
+<input type="number" name="<?=$searchKeyGlobal.'['.$searchKeyDistMtr.'][Max]'?>" value="<?=$_REQUEST[$searchKeyGlobal][$searchKeyDistMtr]['Max']?>" placeholder="Max">
+</fieldset>
+
+<fieldset><legend>Distance to Campus (Minutes by <abbr title="Low-Range Personal Traffic">LPT</abbr>)</legend>
+<input type="number" name="<?=$searchKeyGlobal.'['.$searchKeyDistLPT.'][Min]'?>" value="<?=$_REQUEST[$searchKeyGlobal][$searchKeyDistLPT]['Min']?>" placeholder="Min">
+-
+<input type="number" name="<?=$searchKeyGlobal.'['.$searchKeyDistLPT.'][Max]'?>" value="<?=$_REQUEST[$searchKeyGlobal][$searchKeyDistLPT]['Max']?>" placeholder="Max">
+</fieldset>
+
+
+<fieldset><legend>Price / Week</legend>
+<input type="number" name="<?=$searchKeyGlobal.'['.$searchKeyPrice.'][Min]'?>" value="<?=$_REQUEST[$searchKeyGlobal][$searchKeyPrice]['Min']?>" placeholder="Min">
+-
+<input type="number" name="<?=$searchKeyGlobal.'['.$searchKeyPrice.'][Max]'?>" value="<?=$_REQUEST[$searchKeyGlobal][$searchKeyPrice]['Max']?>" placeholder="Max">
+</fieldset>
+
+
+<fieldset><legend>Filter...</legend>
+<input type="submit" value="Show Me the ResultSet">
+</fieldset>
+
+</form></div><?php
+
+
+
+$maxEntriesPage=10;
+$curPage=1;
+
+
+#var_dump($msdb);
+
+$sqlWhere=[];
+$sqlOrder=[];
+
+if (array_key_exists($searchKeyGlobal,$_REQUEST)) {
+
+	$searchRef=&$_REQUEST[$searchKeyGlobal];
+	
+	if (array_key_exists($searchKeyDistMtr,$searchRef)) {
+		$distRef=&$searchRef[$searchKeyDistMtr];
+		if (count(array_filter($distRef)) > 1) {
+			$sqlWhere[]='entf_meter BETWEEN '.$distRef['Min'].' AND '.$distRef['Max'];
+			$sqlOrder[]='entf_meter ASC';
+		}
+	}
+	
+	if (array_key_exists($searchKeyDistLPT,$searchRef)) {
+		$distRef=&$searchRef[$searchKeyDistLPT];
+		if (count(array_filter($distRef)) > 1) {
+			$sqlWhere[]='entf_min BETWEEN '.$distRef['Min'].' AND '.$distRef['Max'];
+			$sqlOrder[]='entf_min ASC';
+		}
+	}
+	
+	if (array_key_exists($searchKeyPrice,$searchRef)) {
+		$distRef=&$searchRef[$searchKeyPrice];
+		if (count(array_filter($distRef)) > 1) {
+			$sqlWhere[]='preis BETWEEN '.$distRef['Min'].' AND '.$distRef['Max'];
+			$sqlOrder[]='preis ASC';
+		}
+	}
+
+	
+}
+
+
+#print_r($sqlWhere);
+
+$sql='SELECT w.*, v.anrede, v.nname, COUNT(f.m_id) AS cnt FROM wohnung AS w JOIN vermieter AS v ON v.vm_id=w.vm_id LEFT JOIN favorit AS f ON f.wohn_id=w.wohn_id WHERE w.visible > 0 '.(count($sqlWhere) > 0 ? 'AND '.implode('AND',$sqlWhere) : '').' GROUP BY w.wohn_id ORDER BY '.(count($sqlOrder) > 0 ? implode(',',$sqlOrder) : 'cnt DESC').' LIMIT '.(($curPage-1)*$maxEntriesPage).','.$maxEntriesPage;
+$mrs=$msdb->query($sql); echo $msdb->error; // echo $sql;
+
+if ($mrs->num_rows > 0) {
+
+	echo '<table class="resultset" cellpadding="5">';
+	echo '<tr><th colspan="3"></th><th colspan="2">Distance 2 Campus</th></tr>';
+	echo '<tr><th>Image</th><th>Appartment</th><th>Price</th><td>Meters</td><td>Minutes</td><th>Options</th></tr>';
+
+	while ($row=$mrs->fetch_assoc()) {
+		
+		echo '<tr><td>First Image</td><td><a href="?show='.$row['wohn_id'].'">'.$row['name'].'</a><br>'.$row['plz'].' '.$row['ort'].', '.$row['str'].'';
+		echo '<td>'.$row['preis'].'</td><td>'.$row['entf_meter'].'</td><td>'.$row['entf_min'].'</td><td>Bookmark, Chat [...]</td>'."</td></tr>\n";
+		
+	}
+
+	echo '</table>';
+
+
+} else {
+
+
+GUI::printWarn('Keine Ergebnisse');
+
+}
+
+echo '<div id="footer"><a href="about">Profiles (Index)</a> ';
+
+$ourNames = ['manuel'=>"Manuel Schmitt", 'michael'=>"Michael Iglhaut", 'moritz'=>"Moritz Mrosek", 'ramon'=>"Ramon Wilhelm", 'simon'=>"Simon Leister"];
+
+$aboutDir='about/';
+foreach (glob($aboutDir . '*.profile.html') as $phpfile)
+{
+	$fileParts=explode('.',basename($phpfile));
+	echo ' &middot; <a href="' . ($phpfile) . '">' . $ourNames[$fileParts[0]] . '</a>';
+
+}
+
+
+?></div>
+
+
+
+</body></html>
