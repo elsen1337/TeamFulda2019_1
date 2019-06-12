20688942cfd98584ebf5bf7735cbbfb4de0f7187
Michael <Michael.Iglhaut@informatik.hs-fulda.de>
2019-06-12 19:03:01 +0200

    Korrektur letzter commit

--- Seiten/navmenu.php
+++ Seiten/navmenu.php
@@ -1,16 +1,16 @@
-		<header>
-			<div class="top-nav">
-				<div class="logo">
-					<a href="#">
-						<img src="./img/logo-studyhome.png" height="130px" width="192px"/>
-					</a>
-				</div>
-				<div class="menu-top">
-					<a href="#">Vermieten</a>
-					<a href="#">Nachrichten</a>
-					<a href="#">Anmelden</a>
-					<a href="#">Mein Konto</a>
-					<a href="#">Abmelden</a>
-				</div>
-			</div>
-		</header>
+<header>
+    <div class="top-nav">
+        <div class="logo">
+            <a href="#">
+                <img src="./img/logo-studyhome.png" height="130px" width="192px"/>
+            </a>
+        </div>
+        <div class="menu-top">
+            <a href="#">Vermieten</a>
+            <a href="#">Nachrichten</a>
+            <a href="#">Anmelden</a>
+            <a href="#">Mein Konto</a>
+            <a href="#">Abmelden</a>
+        </div>
+    </div>
+</header>
