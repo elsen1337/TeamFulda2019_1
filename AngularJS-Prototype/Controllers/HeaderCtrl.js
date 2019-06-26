4fa7a88b7362c50404c11bbc1f05e2b2d3b8aed9
Moritz <moricie@web.de>
2019-06-26 19:54:10 +0200

    Added search functionality to header.

--- AngularJS-Prototype/Controllers/HeaderCtrl.js
+++ AngularJS-Prototype/Controllers/HeaderCtrl.js
@@ -1,4 +1,8 @@
 studyHomeApp.controller('HeaderCtrl', ['$scope', function($scope){
-
-
+    window.setSearchKeyHeader = () =>
+    {
+        window.startSearch = $scope.startSearchHeader;
+        $scope.startSearchHeader = '';
+        return true;
+    };
 }]);
