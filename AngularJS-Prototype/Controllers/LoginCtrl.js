f27ba9ee02d3ad297fcf7ff1042a4b398a345955
Alan Horman <alanhorman@gmx.de>
2019-06-27 15:43:27 +0200

    Added lines LoginCtrl.js

--- AngularJS-Prototype/Controllers/LoginCtrl.js
+++ AngularJS-Prototype/Controllers/LoginCtrl.js
@@ -1,14 +1,38 @@
-studyHomeApp.controller('LoginCtrl', ['$scope', '$location', function($scope, $location){
+studyHomeApp.controller('LoginCtrl', ['$scope', '$http','$location', function($scope, $http, $location){
     $scope.routes = ROUTES;
 
-    $scope.login = function () {
+    $scope.user = [{}];
+
+    $scope.login = function (Auth) {
         var email = document.getElementById("email").value;
         var passwort = document.getElementById("passwort").value;
         console.log('E-Mail = ' + email + ', Passwort = ' + passwort);
+
+        $scope.user.email = email;
+        $scope.user.passwort = passwort;
+
+        console.log
+
+        $http({
+            url : "../restapi/handler.php?objAction=lessorlogin",
+            method: "POST",
+            // headers : {'Content-Type': 'application/x-www-form-urlencoded'},
+            data : $scope.user
+        }).then(function mySuccess(response) {
+            console.log("Fischkopf");
+            $scope.putSucc = response.data;
+            console.log(response.data);
+            console.log("status: " + response.status);
+            console.log("statusText: " + response.statusText);
+        }, function myError(response) {
+            $scope.error = response.statusText;
+            console.error($scope.error);
+        });
+
+        $location.path('/renting');
     };
 
     $scope.goto = function ( path ) {
         $location.path( path );
     };
 }]);
-
