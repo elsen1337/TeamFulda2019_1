a65f933c48ce6f951b5778dde0df8fd1c1350ef0
Alan Horman <alanhorman@gmx.de>
2019-06-30 13:09:56 +0200

    Fixed Login-Bug. You have to fill out the form first

--- AngularJS-Prototype/Controllers/LoginCtrl.js
+++ AngularJS-Prototype/Controllers/LoginCtrl.js
@@ -14,61 +14,64 @@ studyHomeApp.controller('LoginCtrl', ['$scope', '$http','$location', function($s
 
         console.log('E-Mail = ' + email + ', Passwort = ' + passwort);
 
-        let user = {
-            "email" : $scope.email,
-            "pwort" : $scope.pwort
+
+        var pattern = /^[a-z0-9]*\.[a-z0-9]*@(\b(informatik)\b|\b(cs)\b)\.\b(hs)\b\-\b(fulda)\b\.\b(de)\b/;
+
+        if ($scope.loginForm.$valid) {
+                let user = {
+                    "email": $scope.email,
+                    "pwort": $scope.pwort
+                }
+
+                if ($scope.loginrolle == "Lessor") {
+                    urlVar = "../restapi/handler.php?objAction=lessorlogin";
+                } else if ($scope.loginrolle == "Tenant") {
+                    urlVar = "../restapi/handler.php?objAction=tenantlogin";
+                }
+
+                $http({
+
+                    url: urlVar,
+                    method: "POST",
+                    // headers : {'Content-Type': 'application/x-www-form-urlencoded'},
+                    data: user
+                }).then(function mySuccess(response) {
+                    console.log($scope.loginrolle);
+                    $scope.putSucc = response.data;
+
+                    if (response.data === null) {
+                        $scope.log_mytext = "The password is wrong or the user doesn't exist. Try again and watch of your selected role.";
+                    }
+
+                    if (typeof (Storage) !== "undefined" && $scope.putSucc !== null) {
+                        // Store
+                        sessionStorage.setItem("isLoggedIn", "yes")
+                        sessionStorage.setItem("vm_id", $scope.putSucc["vm_id"]);
+                        sessionStorage.setItem("vname", $scope.putSucc["vname"]);
+                        sessionStorage.setItem("nname", $scope.putSucc["nname"]);
+                        sessionStorage.setItem("role", $scope.loginrolle);
+                        // Retrieve
+
+                        $scope.log_mytext = "";
+
+
+                        console.log(response.data);
+                        //console.log($scope.putSucc["vname"]);
+                        console.log("status: " + response.status);
+                        console.log("statusText: " + response.statusText);
+                        window.location.href = "./index.html#!/homeStart";
+                        window.location.reload();
+                    }
+                    // $location.path("index.html#!/homeStart");
+                }, function myError(response) {
+                    $scope.error = response.statusText;
+                    console.error($scope.error);
+                });
         }
-
-        console.log(user);
-
-        if ($scope.loginrolle == "Lessor")
+        else
         {
-            urlVar = "../restapi/handler.php?objAction=lessorlogin";
+            $scope.log_mytext = "Please, fill out the formular correktly.";
         }
-        else if ($scope.loginrolle == "Tenant")
-        {
-            urlVar = "../restapi/handler.php?objAction=tenantlogin";
-        }
-
-        $http({
-
-            url : urlVar,
-            method: "POST",
-            // headers : {'Content-Type': 'application/x-www-form-urlencoded'},
-            data : user
-        }).then(function mySuccess(response) {
-            console.log($scope.loginrolle);
-            $scope.putSucc = response.data;
-
-            if (response.data === null)
-            {
-                $scope.log_mytext = "The password is wrong or the user doesn't exist. Try again and watch of your selected role.";
-            }
-
-            if (typeof(Storage) !== "undefined" && $scope.putSucc !== null) {
-                // Store
-                sessionStorage.setItem("isLoggedIn", "yes")
-                sessionStorage.setItem("vm_id", $scope.putSucc["vm_id"]);
-                sessionStorage.setItem("vname", $scope.putSucc["vname"]);
-                sessionStorage.setItem("nname", $scope.putSucc["nname"]);
-                sessionStorage.setItem("role", $scope.loginrolle);
-                // Retrieve
-
-                $scope.log_mytext = "";
-
-
-                console.log(response.data);
-                //console.log($scope.putSucc["vname"]);
-                console.log("status: " + response.status);
-                console.log("statusText: " + response.statusText);
-                window.location.href = "./index.html#!/homeStart";
-                window.location.reload();
-            }
-           // $location.path("index.html#!/homeStart");
-        }, function myError(response) {
-            $scope.error = response.statusText;
-            console.error($scope.error);
-        });
     };
 
     $scope.goto = function ( path ) {
