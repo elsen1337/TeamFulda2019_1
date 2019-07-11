b2d2bac5f16e0a6ccf61052a59cf1ab419d4d6b4
Michael <michael.iglhaut@informatik.hs-fulda.de>
2019-07-11 12:17:34 +0200

    Favoriten entfernen hinzugefügt

--- AngularJS-Prototype/Controllers/FavouritesCtrl.js
+++ AngularJS-Prototype/Controllers/FavouritesCtrl.js
@@ -37,4 +37,32 @@ studyHomeApp.controller('FavouritesCtrl', ['$http', '$scope', '$location', funct
         $location.path("details?id=" + id);
         console.log("Hallo:" + id);
     }
+
+    let url2 = `../restapi/handler.php?objAction=tenantfavorit`;
+    $scope.delete = (id, evt) => {
+        let data = JSON.stringify({
+            "wohn_id": id,
+            "m_id": sessionStorage.getItem('m_id')
+        });
+
+        $http.delete(url2, data,
+            {
+                transformRequest: angular.identity,
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then((response) =>
+                {
+                    $scope.searchData = response.data;
+                    console.log(response.data);
+                    console.log("status: " + response.status);
+                    console.log("statusText: " + response.statusText);
+                    //reset search items because sth could be left over
+                },
+                (err) => {
+                    console.log(err);
+                });
+    }
 }]);
+
