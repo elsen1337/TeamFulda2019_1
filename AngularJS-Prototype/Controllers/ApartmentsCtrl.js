5dc6b97ad938a35b525f728412619696150ed02c
Moritz <moricie@web.de>
2019-07-13 21:12:00 +0200

    Added Edit Apartment page and changed apartments page

--- AngularJS-Prototype/Controllers/ApartmentsCtrl.js
+++ AngularJS-Prototype/Controllers/ApartmentsCtrl.js
@@ -1,4 +1,46 @@
 studyHomeApp.controller('ApartmentsCtrl', ['$scope', '$http', function($scope, $http) {
 
+    console.log(sessionStorage.getItem('vm_id'));
+    console.log(sessionStorage.getItem('vname'));
+    let url = `../restapi/handler.php?objAction=lessorapartment&objKey=${sessionStorage.getItem('vm_id')}`;
+
+    $scope.getApartments = () => {
+        $http.get(url,
+            {
+                transformRequest: angular.identity,
+                headers: {
+                    'Content-Type': undefined
+                }
+            })
+            .then((response) =>
+                {
+                    $scope.searchData = response.data;
+                    console.log(response.data);
+                    console.log("status: " + response.status);
+                    console.log("statusText: " + response.statusText);
+                    //reset search items because sth could be left over
+
+                    $scope.searchItems = [{}];
+                    if($scope.searchData.length === 0) {
+                        document.getElementById("searchList").style.display="none";
+                    }
+                    for(let i = 0; i < $scope.searchData.length; i++) {
+                        $scope.searchItems[i] = {
+                            id : $scope.searchData[i].wohn_id,
+                            alt : $scope.searchData[i].imgalt,
+                            name : $scope.searchData[i].name,
+                        };
+                    }
+                },
+                (err) => {
+                    console.log(err);
+                });
+    }
+    $scope.getApartments();
+
+    $scope.goToDetails2 = (id, evt) => {
+        $location.path("details?id=" + id);
+        console.log("Hallo:" + id);
+    }
 
 }]);
