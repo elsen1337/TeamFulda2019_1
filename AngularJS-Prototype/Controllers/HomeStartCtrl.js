c3bd2d140f27db32b5cbac0819514b52d7fd9f1e
Michael <michael.iglhaut@informatik.hs-fulda.de>
2019-07-18 14:03:24 +0200

    Test9

--- AngularJS-Prototype/Controllers/HomeStartCtrl.js
+++ AngularJS-Prototype/Controllers/HomeStartCtrl.js
@@ -1,7 +1,23 @@
 studyHomeApp.controller('HomeStartCtrl', ['$scope', '$http', '$location', function($scope, $http, $location){
     window.setSearchKey = () =>
     {
-        window.startSearch = $scope.startSearch;
+        let result = {"appsearch[fulltext]" : $scope.startSearch};
+
+        $http({
+            url : "../restapi/handler.php?objAction=estatesearch",
+            method: "PUT",
+            headers : {'Content-Type': undefined},
+            data : result
+        }).then(function mySuccess(response) {
+            $scope.putSucc = response.data;
+            console.log(response.data);
+            console.log("status: " + response.status);
+            console.log("statusText: " + response.statusText);
+
+        }, function myError(response) {
+            $scope.error = response.statusText;
+            console.error($scope.error);
+        });
         return true;
     };
 }]);
