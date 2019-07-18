eb99f68048e5c72b1cce9e242a72213c85199a97
Michael <michael.iglhaut@informatik.hs-fulda.de>
2019-07-18 14:10:02 +0200

    Headersuche gefixt

--- AngularJS-Prototype/Controllers/HeaderCtrl.js
+++ AngularJS-Prototype/Controllers/HeaderCtrl.js
@@ -1,8 +1,24 @@
 studyHomeApp.controller('HeaderCtrl', ['$scope', function($scope){
     window.setSearchKeyHeader = () =>
     {
-        window.startSearch = $scope.startSearchHeader;
-        $scope.startSearchHeader = '';
+        let formData = new FormData();
+        formData.append("appsearch[fulltext]", $scope.startSearchHeader);
+
+        $http({
+            url : "../restapi/handler.php?objAction=estatesearch",
+            method: "PUT",
+            headers : {'Content-Type': undefined},
+            data : formData
+        }).then(function mySuccess(response) {
+            $scope.putSucc = response.data;
+            console.log(response.data);
+            console.log("status: " + response.status);
+            console.log("statusText: " + response.statusText);
+
+        }, function myError(response) {
+            $scope.error = response.statusText;
+            console.error($scope.error);
+        });
         return true;
     };
 }]);
