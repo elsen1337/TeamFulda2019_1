cb4d904edcf36d44008da66c7955705fc85c792d
Moritz <moritz.mrosek@informatik.hs-fulda.de>
2019-08-06 16:32:44 +0200

    added connection configuration for aws database

--- core-mysqla.php
+++ core-mysqla.php
@@ -1,7 +1,7 @@
 <?php
 
 
-$msdb=mysqli_connect('localhost','root','root','hsftp_booking');
+$msdb=mysqli_connect('studyhomedb.cqvrg2kdtx7n.us-east-1.rds.amazonaws.com:3306','root','rootroot','hsftp_booking');
 $msdb->set_charset('utf8');
 
 // Hat was mit zulassbaren NULL Werten in Saplten beim Einfügen zu tun; entspricht der Aufhebung der standardmäßig aktivierten strikten Auslegung des Schemas
