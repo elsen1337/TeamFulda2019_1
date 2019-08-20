da7ba66b9ad2190a6769f2df51f7b4bb4b64897a
Alan Horman <alanhorman@gmx.de>
2019-08-20 22:11:25 +0200

    Added function for calling the appointments for lessors

--- AngularJS-Prototype/Controllers/ViewingAppointmentsCtrl.js
+++ AngularJS-Prototype/Controllers/ViewingAppointmentsCtrl.js
@@ -1,4 +1,19 @@
 studyHomeApp.controller('ViewingAppointmentsCtrl', ['$scope', '$http', function($scope, $http) {
 
 
+    $scope.viewAppointments = function () {
+        id = sessionStorage.getItem("vm_id")
+        console.log('tetetetetet');
+
+            $http({
+                url: `../restapi/handler.php?objAction=estatemeeting&objKey=${1}`,
+                method: "GET",
+                // headers : {'Content-Type': 'application/x-www-form-urlencoded'},
+            }).then(function mySuccess(response) {
+                $scope.putSucc = response.data;
+                console.log($scope.putSucc)
+            }, function myError(response) {
+                console.log(response);
+            });
+    };
 }]);
