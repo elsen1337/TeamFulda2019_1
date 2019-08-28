21c81993c17c00f5202e9f76abefa0b4e17e332c
Michael <michael.iglhaut@informatik.hs-fulda.de>
2019-08-28 12:47:53 +0200

    Globale Referenzierung

--- restapi/mod_rating_michael.php
+++ restapi/mod_rating_michael.php
@@ -2,7 +2,7 @@
 
     function ratingList($vmid) {
 		$sql='SELECT * FROM v_rating WHERE vm_id='.$vmid.'';
-		$mrs=$GLOBALS[self::$dbvar]->query($sql);
+		$mrs=$GLOBALS['msdb']->query($sql);
 		$attrarr=[];
 		while ($obj=$mrs->fetch_object()) {
 			$attrarr[]=$obj;
@@ -13,8 +13,8 @@
     function ratingAddUpdate($vm_id,$m_id,$stars,$cmt,$scr=null) {
 		if (strlen($scr)==0) {$scr='null';}
         $sql='INSERT INTO v_rating (vm_id, m_id, stars, cmt) VALUES ('.$vm_id.', '.$m_id.', '.$stars.', '.$cmt.')';
-        $mrs=$GLOBALS[self::$dbvar]->query($sql);
-        return $GLOBALS[self::$dbvar]->affected_rows > 0; // !!! Spezialfall 0,1(Insert),2(Update)
+        $mrs=$GLOBALS['msdb']->query($sql);
+        return $GLOBALS['msdb']->affected_rows > 0; // !!! Spezialfall 0,1(Insert),2(Update)
     }
 
     if ($_SERVER['REQUEST_METHOD']=='GET') {
